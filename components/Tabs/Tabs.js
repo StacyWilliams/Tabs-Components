28f130e66a337220dd2686dc37b2903b409296f1
Stacy Williams <49852311+StacyWilliams@users.noreply.github.com>
2019-05-16 17:12:06 -0500

    MVP

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -9,12 +9,12 @@ class TabLink {
       this.data = this.element.dataset.tab
     // Using the custom data attribute get the associated Item element
     // this.itemElement;
-    this.itemElement = this.element.querySelector(`.tabs-item[data-tab=${this.data}` )
+    this.itemElement = document.querySelector(`.tabs-item[data-tab='${this.data}']` )
     // Using the Item element, create a new instance of the TabItem class
     // this.tabItem;
     this.tabItem = new TabItem(this.itemElement)
     // Add a click event listener on this instance, calling the select method on click
-      this.tabItem.addEventListener('click', () => this.select() )
+      this.element.addEventListener('click', () => this.select())
   };
 
   select() {
@@ -22,7 +22,7 @@ class TabLink {
 
     // Get all of the elements with the tabs-link class
     // const links;
-             const links = document.querySelectorAll('tabs-link')
+          const links = document.querySelectorAll('tabs-link')
     // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
     // Array.from(links).forEach();
 
@@ -49,10 +49,10 @@ class TabItem {
     
     // Select all ".tabs-item" elements from the DOM
     // const item
-    const items = document.querySelectorAll('.tab-items')
+    let items = document.querySelectorAll('.tabs-item')
 
     // Remove the class "tabs-item-selected" from each element
-       Array.from(items).forEach(item => {
+         items.forEach(item => {
          item.classList.remove('tabs-item-selected')
        })
     // Add a class named "tabs-item-selected" to this element
@@ -71,9 +71,7 @@ class TabItem {
 
 */
 
-links = document.querySelectorAll('.tabs-link');
-
-links = document.querySelectorAll('.tab-link');
-Array.from(links).forEach(link =>{
+let links = document.querySelectorAll('.tabs-link');
+links.forEach(link =>{
  new TabLink(link);
 });
