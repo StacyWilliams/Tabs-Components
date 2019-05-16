a7e3722c986368296e3e6679be0e3fe26ac0622e
Stacy Williams <49852311+StacyWilliams@users.noreply.github.com>
2019-05-16 17:55:01 -0500

    now it is totally correct

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -14,15 +14,15 @@ class TabLink {
     // this.tabItem;
     this.tabItem = new TabItem(this.itemElement)
     // Add a click event listener on this instance, calling the select method on click
-      this.element.addEventListener('click', () => this.select())
+      this.element.addEventListener('click', () => {this.select(element)})
   };
 
-  select() {
+  select(element) {
 
 
     // Get all of the elements with the tabs-link class
     // const links;
-          const links = document.querySelectorAll('tabs-link')
+          let links = document.querySelectorAll('.tabs-link')
     // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
     // Array.from(links).forEach();
 
